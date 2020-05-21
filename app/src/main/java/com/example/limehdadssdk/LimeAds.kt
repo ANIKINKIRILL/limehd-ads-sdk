f99fdc4abda88a675a057c226bc60c3f66c4627d
ANIKINKIRILL <ky.anikin@mail.ru>
2020-05-21 13:43:08 +0300

    Добавил LimeAds класс
    В этом классе будет происходить осн. логика и работа с рекламой от моб. медиации

---
+++ app/src/main/java/com/example/limehdadssdk/LimeAds.kt
@@ -0,0 +1,17 @@
+package com.example.limehdadssdk
+
+import android.content.Context
+
+/**
+ * Класс для работы с рекламой
+ *
+ * @param context   Context приложения
+ * @param json      Json, который даёт сервер. Со всеми необходимыми объектами
+ */
+
+class LimeAds constructor(private val context: Context, private val json: String) {
+
+    
+
+}
+
